ef7b23cef92a56b53491a0dee792d49d0e0db952
Rui Wang <32067492+RuiWang1998@users.noreply.github.com>
2019-04-11 00:48:08 -0400

    Improved file structure and whatnot
    The model_prepare.py file contains the class that I generally use for both visualization and sensitivity calculation

--- Sensitivity.py
+++ Sensitivity.py
@@ -8,146 +8,19 @@ import gc
 
 import torch
 
+from model_prepare import PreModel
 
-class SensitivityMeasurer:
+
+class SensitivityMeasurer(PreModel):
     """
     This class serves as a sensitivity measurer that has two metrics from the paper
     Sensitivity and Generalization in Neural Networks: an Empirical Study
     """
     def __init__(self, model, module_list=None, cuda=True, model_intake_size=416, batch_size=1, channel_num=3):
-        """
-        This function initializes the object
-        :param model: the model of interest
-        :param module_list: the module list of the model, if provided, otherwise will be written by list(model.children())
-        :param cuda: whether to use cuda, which will be limited by hardware
-        :param model_intake_size: the input size of the image, could be a int or a list/tuple
-        :param batch_size: the batch size of the input
-        :param channel_num: the number of input channel
-        """
-        # check if use cuda
-        self.cuda = cuda and torch.cuda.is_available()
-        self.device = torch.device('cuda:0' if self.cuda else 'cpu')
-
-        # get the models and module list
-        if model is not None:
-            self.model = model.to(self.device)
-            self.model.eval()
-        if module_list is not None:
-            self.module_list = module_list
-        else:
-            self.module_list = list(model.children())
-        for i, layer in enumerate(self.module_list):
-            self.module_list[i] = layer.to(self.device)
-
-        self.layer_num = len(self.module_list)  # the number of layers of the model
-        self.channel_count = []  # the number of channels of each layer
-        self.size_count = []  # elements of format [[height, width]]
-        self.batch_size = batch_size  # the batch size of the input to the model
-        self.channel_num = channel_num
-
-        if isinstance(model_intake_size, int):
-            self.width = model_intake_size
-            self.height = model_intake_size
-        else:
-            self.width, self.height = model_intake_size
-
-        self._module_list_channel_count()
-
+        super(SensitivityMeasurer, self).__init__(model, module_list, cuda, model_intake_size, batch_size, channel_num)
         gc.collect()
-        torch.cuda.empty_cache()
-
-    def _module_list_channel_count(self):
-        """
-        This function counts the number of channels in each layer
-        Note that for any model with residue network, we have to overload this method
-        """
-        place_holder = torch.randn(self.batch_size, self.channel_num,
-                                   self.height, self.width).to(self.device)
-        for i, layer in enumerate(self.module_list):
-            self.size_count.append(0)
-            self.module_list[i] = layer.to(self.device)
-            try:
-                place_holder = self.module_list[i](place_holder)
-                output_shape = place_holder.shape
-                self.channel_count.append(output_shape[1])
-                for channel_idx in range(output_shape[1]):
-                    self.size_count[-1] = output_shape[2:]
-            except NotImplementedError:
-                self.size_count[-1] = torch.Size([0, 0])
-
-    def forward_pass(self, img, layer_idx):
-        """
-        This function gets the nth layer output
-        :param img: the input image
-        :param layer_idx: the layer index of interest
-        :return: the output at that layer
-        """
-        img = img.to(self.device)
-        for i, layer in enumerate(self.module_list):
-            try:
-                img = layer(img)
-            except NotImplementedError:
-                pass
-            if i == layer_idx:
-                return img
-        raise ValueError(f"Layer {layer_idx} is larger than the maximum layer count {self.layer_num}")
-
-    @staticmethod
-    def reduction(reduction_tensor, reduction_type):
-        """
-        This function does the dimensionality reduction
-        :param reduction_tensor: the tensor to reduce dimensionality of
-        :param reduction_type: the type of reduction to perform, one of 'mean' or 'sum'
-        :return: the reduced tensor
-        """
-        if reduction_type is None:
-            return reduction_tensor
-        if reduction_type == 'mean':
-            return reduction_tensor.mean()
-        if reduction_type == 'sum':
-            return reduction_tensor.sum()
-
-    def get_n_th_layer(self, img, layer_idx, reduction=None):
-        """
-        This function wraps around the get_n_th_layer_act_core function to introduce the ability of reduction
-        :param img: the input image
-        :param layer_idx: the the index of the layer of interest
-        :param reduction: the reduction method, one of 'sum' or 'mean'
-        :return: the n-th layer's activation
-        """
-        return self.reduction(self.forward_pass(img, layer_idx), reduction)
-
-    def get_nth_channel(self, img, layer_idx, channel_idx, reduction=None):
-        """
-        This function gets the n-th channel of the n-th layer's output
-        :param img: the input image
-        :param layer_idx: the layer index
-        :param channel_idx: the channel index
-        :param reduction: the reduction method, one of 'sum' or 'mean'
-        :return: the n-th channel output
-        """
-        try:
-            return self.reduction(self.get_n_th_layer(img, layer_idx)[:, channel_idx, :, :], reduction)
-        except IndexError:
-            try:
-                return self.reduction(self.get_n_th_layer(img, layer_idx)[:, channel_idx, :], reduction)
-            except IndexError:
-                pass
-
-    def get_nth_neuron(self, img, layer_idx, channel_idx, neuron_idx):
-        """
-        This function gets the n-th neuron of the n-th channel of the n-th layer's output
-        :param img: the input image
-        :param layer_idx: the index of the layer of interest
-        :param channel_idx: the index of the channel of interest
-        :param neuron_idx: the index or the coordinate of the neuron of interest
-        :return: the activation of that neuron
-        """
-        if isinstance(neuron_idx, list) or isinstance(neuron_idx, tuple):
-            x_idx, y_idx = neuron_idx
-        else:
-            x_idx = y_idx = neuron_idx
-        return self.get_nth_channel(img, layer_idx, channel_idx, reduction=None)[:, x_idx, y_idx]
+        if cuda:
+            torch.cuda.empty_cache()
 
     def compute_neuron_jacobian(self, inputs, layer_idx, channel_idx, neuron_idx):
         """
@@ -243,3 +116,15 @@ class SensitivityMeasurer:
                                                                   layer_idx, channel_idx, mode=mode).clone().cpu())
                     torch.cuda.empty_cache()
             return jacobian
+
+    def gradient_incr(self, inputs):
+        """
+        This function computes the gradient from adjacent layers
+        :param inputs: the input
+        :return: the gradients
+        """
+        inputs = inputs.to(self.device)
+        gradients = []
+        for i in range(self.layer_num):
+            gradients.append(self.forward_pass(inputs, input_index=i, output_index=i + 1))
+        return gradients
--- YOLOSensitivity.py
+++ YOLOSensitivity.py
@@ -9,7 +9,7 @@ import numpy as np
 import torch
 
 from Sensitivity import SensitivityMeasurer
-from YOLOv3.models import Darknet
+from modified_YOLO import YOLO
 
 
 class YOLOMeasurer(SensitivityMeasurer):
@@ -26,15 +26,22 @@ class YOLOMeasurer(SensitivityMeasurer):
         """
         super(YOLOMeasurer, self).__init__(model, module_list, cuda=cuda)
 
-    def forward_pass(self, img, idx):
+    def forward_pass(self, img, layer_idx=None, output_index=None, input_index=None):
         """
         This function gets the n-th layer output of YOLOv3
         :param img: the input image
-        :param idx: the index of the layer
+        :param layer_idx: the index of the layer
+        :param output_index: the index of the output to take gradient from
+        :param input_index: the index of the input layer whose gradient is calculated
         :return: the output of the specific layer
         """
-        img = img.to(self.device)
-        return self.model(img, layer_idx=[idx])[idx]
+        get_grad = output_index is not None and input_index is not None
+        if not get_grad:
+            img = img.to(self.device)
+            return self.model(img, layer_idx=[layer_idx])[layer_idx]
+        else:
+            img = img.to(self.device)
+            return self.model(img, output_index=output_index, input_index=input_index)
 
     def _module_list_channel_count(self):
         """
@@ -70,7 +77,7 @@ if __name__ == '__main__':
     img2 = cv2.resize(img2, tuple([image_size, image_size]))
     img2 = torch.tensor(img2 / 255, dtype=torch.float32).permute(2, 0, 1).unsqueeze(0)
 
-    YOLOv3 = Darknet(config_path, image_size)
+    YOLOv3 = YOLO(config_path, image_size)
     YOLOv3.load_weights(weight_path)
     yolo_module_list = list(YOLOv3.children())[0]
     place_holder = torch.randn(1, 3, 416, 416).to(device)
@@ -78,7 +85,26 @@ if __name__ == '__main__':
     # measurer.get_nth_neuron(place_holder, 1, 0, 0).shape
     # Jacob = measurer.compute_channel_jacobian(place_holder, 1, 0)
     # jacobian = measurer.compute_channel_jacobian(img1, [0, 0], "reduction_mean")
+    gradients1 = measurer.gradient_incr(img1)
+    mean_gradients1 = []
+    stacked_grad = []
+    for i, gradient in enumerate(gradients1):
+        if gradient is not None:
+            grad_list = [gradient[0, i, ...] for i in range(gradient.shape[1])]
+            stacked_grad += grad_list
+    mean_gradients1 = [gradient.norm().detach().cpu().numpy() for gradient in stacked_grad]
 
+    gradients2 = measurer.gradient_incr(img2)
+    mean_gradients2 = []
+    stacked_grad = []
+    for i, gradient in enumerate(gradients2):
+        if gradient is not None:
+            grad_list = [gradient[0, i, ...] for i in range(gradient.shape[1])]
+            stacked_grad += grad_list
+    mean_gradients2 = [gradient.norm().detach().cpu().numpy() for gradient in stacked_grad]
+
+    np.savetxt("pair_compare/incr_1.csv", np.array(mean_gradients1), delimiter=",")
+    np.savetxt("pair_compare/incr_2.csv", np.array(mean_gradients2), delimiter=",")
     """
     uncomment this part if you want to compute the norm
     
