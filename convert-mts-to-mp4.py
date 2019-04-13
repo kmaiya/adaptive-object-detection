7e247c9f076e247fcfbf53cce3adc40156c28ba0
Daniel Merrick <dmerrick@purdue.edu>
2019-04-12 22:13:56 -0400

    removing duplicate

--- convert-mts-to-mp4.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-infile = 'engineering-fountain-outside-nonstationary/engineering-fountain-outside-nonstationary.MTS'
-outfile = infile[:-3] + 'mp4'
-
-subprocess.run(['ffmpeg', '-i', infile, outfile])
